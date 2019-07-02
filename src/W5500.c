2d881b6012dc5dcd4a82ac9b0a576f470807345d
loti1990 <lotnik.loti@gmail.com>
2019-07-02 21:22:13 +0200

    Added comends for initialization Socket 0, RX&TX memory alocation, port number, protocol, and send open command

--- src/W5500.c
+++ src/W5500.c
@@ -25,6 +25,37 @@ uint8_t CR_SHAR_W5500[9] = {0x00,0x09,0x04,0x00,0x08,0xdc,0x01,0x02,0x03};
 //source IP address = 192(0xc0).168(0xa8).1(0x01).100(0x64)
 uint8_t CR_SIPR_W5500[7] = {0x00,0x0f,0x04,192,168,1,100};
 
+//common register - PHY configuration register PHYCFGR
+//no reset
+//PHY operation mode configure with OPMDC[2:0]avi
+//Operation mode: 100BT Full- duplex, auto negotiation disabled
+uint8_t CR_PHYCFGR_W5500[4] = {0x00,0x2e,0x04,0xd8};
+
+////////////
+//SOCKET 0//
+////////////
+
+//Socket 0 RX and TX buffer size register
+//RX memory allocation size 2kBytes
+//TX memory allocation size 2kBytes
+uint8_t S0_RX_TX_BUF_SIZE_W5500[5] = {0x00,0x1e,0x0c,0x02,0x02};
+
+//Socket 0 mode register
+//Socket 0 used TCP protocol
+uint8_t S0_MR_W5500[4] = {0x00,0x00,0x0c,0x01};
+
+//Socket 0 source port
+//COnfigure source port as 1024
+uint8_t S0_PORT_W5500[5] = {0x00,0x04,0x0c,0x04,0x00};
+
+//Socket 0 open
+//OPEN
+uint8_t S0_CR_OPEN_W5500[4] = {0x00,0x01,0x0c,0x01};
+
+
+
+
+
 //Initialize W5500 ethernet module
 void W5500Init(void){
 
@@ -42,5 +73,25 @@ void W5500Init(void){
 
 	//setup source IP address register
 	SPI1SendNByte(CR_SIPR_W5500,7);
+
+	//setup PHY configuration register
+	SPI1SendNByte(CR_PHYCFGR_W5500,4);
+
+	//setup Socket 0 RX and TX memory allocation
+	SPI1SendNByte(S0_RX_TX_BUF_SIZE_W5500,5);
+
+	//setup Socket 0 Protocol
+	SPI1SendNByte(S0_MR_W5500,4);
+
+	//setup Socket 0 port number
+	SPI1SendNByte(S0_PORT_W5500,5);
+
+	//open Socket 0
+	SPI1SendNByte(S0_CR_OPEN_W5500,4);
+
+	//to do
+	//preveri INIT flag v status tegistru od socket 0 (preverjaj v while zanki)
+
+	//pošlji ukaz za "LISTEN" DS-46
 
 }
