deb4d4f2d03d0eded52438c66af86f67a5d9d283
loti1990 <lotnik.loti@gmail.com>
2020-03-31 21:05:57 +0200

    added python programs

---
+++ Python_programs/W5500_con/serial_t.py
@@ -0,0 +1,31 @@
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=0.5)  # open serial port
+print(ser.name)         # check which port was really opened
+
+while(1):
+
+	try:
+		data = ser.read(1)
+		#print("Data: %b"%(data.decode()))
+		if(data != b''):
+			print(bin(ord(data.decode())))
+
+	except serial.SerialTimeoutException as e:
+		print("Timeout %s"%e)
+	#	pass
+
+	except KeyboardInterrupt:
+		ser.close()
+		break
+
+ser.close()
+#while(1):
+#	ser.flushInput()
+#	ser.flushOutput()
+#	ser.write(b'00011110')     	# write a string
+#	data = ser.read(2)
+#	adc_val = data[1]<<8|data[0]
+#	voltage = (3./2**12)*adc_val
+#	print("Voltage: %.2f"%(voltage))
+#ser.close()             # close port
---
+++ Python_programs/W5500_con/w5500_con.py
@@ -0,0 +1,58 @@
+import socket
+import time
+IP 	= '192.168.1.100'
+PORT 	= 1024
+
+
+class W5500Conn():
+
+	def __init__(self,ip,port):
+		self.ip 	= ip
+		self.port 	= port
+
+	def OpenTCP(self):
+
+		try:
+			self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			self.s.settimeout(5) 	#timeout 5s
+			self.s.connect((self.ip,self.port))
+			self.s.settimeout(None) #reset timeout to default
+			print("[+] Susesfully connect to:\nIP: %s\nPORT: %s"%(self.ip,self.port))
+		except socket.error as e:
+			print("[-] Fail to open connection on:\nIP: %s\nPORT: %s\nERROR: %s"%(self.ip,self.port,e))
+
+	def CloseTCP(self):
+
+		try:
+			self.s.close()
+			print("[+] Susesfully close connection on:\nIP: %s\nPORT: %s"%(self.ip,self.port))
+
+		except socket.error as e:
+			print("[-] Fail to close connection on:\nIP: %s\nPORT: %s\nERROR: %s"%(self.ip,self.port,e))
+
+	def SendData(self,data):
+
+		try:
+
+			self.s.send(data)
+
+		except socket.error as e:
+
+			print("[-] Fail to send data to:\nIP: %s\nPORT: %s\nERROR: %s"%(self.ip,self.port,e))
+
+def main():
+
+	a = W5500Conn(ip = IP, port = PORT)
+	a.OpenTCP()
+	data = ""
+	for i in range(999):
+		data = data + "1"
+	print(len(data))
+	a.SendData(data.encode())
+	time.sleep(5)
+	a.CloseTCP()
+
+#Run main proram
+if(__name__ == "__main__"):
+
+	main()
